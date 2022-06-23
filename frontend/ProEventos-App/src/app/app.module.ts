223784777cef1097b1fec7e203577037fc0ddee0
Ricardo Luiz Vieira <ricardoluizv@gmail.com>
2022-06-22 21:46:53 -0300

    Install bootstrap in angular. Video 36

--- frontend/ProEventos-App/src/app/app.module.ts
+++ frontend/ProEventos-App/src/app/app.module.ts
@@ -6,10 +6,11 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { EventosComponent } from './eventos/eventos.component';
 import { PalestrantesComponent } from './palestrantes/palestrantes.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [AppComponent, EventosComponent, PalestrantesComponent],
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule, BrowserAnimationsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
